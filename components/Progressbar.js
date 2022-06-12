99cf8312e2a5732aa74c248d5be2ba635aad4c12
KARANDEEP SINGH <50829119+kdsinghcoder@users.noreply.github.com>
2022-06-12 21:29:03 +0530

    basic setup

--- components/Progressbar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Progress from "@material-tailwind/react/Progress";
-
-export default function Progress(props) {
-    return (
-        <Progress color="green" value={props.value} percentage={false} />
-    );
-}
